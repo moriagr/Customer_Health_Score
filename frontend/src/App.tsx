2db6bb5be1c35ad993ff5173172134db9698a47e
moriagr <moriagro@gmail.com>
2025-09-11 11:00:01 +0300

    First commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Dashboard } from './components/Dashboard/Dashboard';
+import { Header } from './components/Layout/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Dashboard />
+    </>
+  );
+}
+
+export default App;
