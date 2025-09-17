5e33eca0dee6e6582bf7772ab91326449b329364
moriagr <moriagro@gmail.com>
2025-09-17 10:57:24 +0300

    Initial commit

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,14 +4,16 @@ dotenv.config();
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const pool = new Pool({
+const pool = new Pool(isProduction ? {
   connectionString: process.env.DATABASE_URL || undefined, // production
-  user: isProduction ? undefined : process.env.DB_USER,   // dev
-  password: isProduction ? undefined : process.env.DB_PASS,
-  host: isProduction ? undefined : process.env.DB_HOST,
-  port: isProduction ? undefined : Number(process.env.DB_PORT),
-  database: isProduction ? undefined : process.env.DB_NAME,
-  ssl: isProduction ? { rejectUnauthorized: false } : false
+  ssl: { rejectUnauthorized: false }
+
+} : {
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  database: process.env.DB_NAME,
 });
 
 // const pool = new Pool({
